25bc6258cc172eb5153d715b37d7c91b17e1f374
bhushan-r-mahajan <bhushanmahajan1997@gmail.com>
2021-04-19 11:30:03 +0530

    Added multiplication of 2 numbers

--- calculator.sh
+++ calculator.sh
@@ -2,6 +2,8 @@ echo "welcome"
 num1=10
 num2=20
 sum=$(($num1+$num2))
-echo "The sum of 2 numbers is"$sum
+echo "The Addition is "$sum
 sub=$(($num2-$num1))
-echo $sub
+echo "The Subtraction is "$sub
+mul=$(($num1*$num2))
+echo "The Multiplication is "$mul
